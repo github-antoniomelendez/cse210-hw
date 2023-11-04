502ad7b94f42621d75bda1be2d76c2bdc00513da
tonymelendez1@hotmail.com <tonymelendez1@hotmail.com>
2023-11-03 20:30:06 -0600

    Finished Prep 1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        // Ask user for their first name.
+        Console.Write("What is your first name? ");
+        string firstName = Console.ReadLine();
+
+        // Ask user for their last name.
+        Console.Write("What is your last name? ");
+        string lastName = Console.ReadLine();
+
+        // Print name like in James Bond movies.
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
     }
 }
